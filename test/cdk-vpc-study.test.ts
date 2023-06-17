ec1da926a49985672bbcba3bc97aec6809419d3c
atsupi <atsupi@gmail.com>
2023-06-17 13:22:27 +0900

    Initial commit

---
+++ test/cdk-vpc-study.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVpcStudy from '../lib/cdk-vpc-study-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-vpc-study-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVpcStudy.CdkVpcStudyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
